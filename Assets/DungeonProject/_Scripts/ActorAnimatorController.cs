6e7cfd23f5a62880e63b60f57170db55c33adc49
gameoflord012 <manhhieu012@gmail.com>
2021-08-08 11:20:58 +0700

    Player movement animation

---
+++ Assets/DungeonProject/_Scripts/ActorAnimatorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ActorAnimatorController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetMovementParameter(bool isWalk)
+    {
+        animator.SetBool("isWalk", isWalk);
+    }
+}
--- Assets/DungeonProject/_Scripts/ActorMovement.cs
+++ Assets/DungeonProject/_Scripts/ActorMovement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class ActorMovement : MonoBehaviour
 {
     [SerializeField]
     private float maxSpeed = 5, acceleration = 5, deacceleration = 8;
+
+    public UnityEvent<bool> OnActorMoving;    
 
     private Vector2 direction;
     private Rigidbody2D rb;
@@ -26,10 +29,12 @@ public class ActorMovement : MonoBehaviour
         if(direction.sqrMagnitude > 0)
         {
             rb.velocity = Vector2.Lerp(rb.velocity, direction * maxSpeed, acceleration * Time.fixedDeltaTime);
+            OnActorMoving?.Invoke(true);
         }
         else
         {
             rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, deacceleration * Time.fixedDeltaTime);
-        }        
+            OnActorMoving?.Invoke(false);
+        }
     }
 }
