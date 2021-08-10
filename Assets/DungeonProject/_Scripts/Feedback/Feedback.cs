ce4563278745bc1d560e2a19f856332d775025c7
gameoflord012 <manhhieu012@gmail.com>
2021-08-10 11:15:46 +0700

    feedback system

---
+++ Assets/DungeonProject/_Scripts/Feedback/Feedback.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class Feedback
+{
+    public abstract void CreateFeedback();
+    public abstract void ResetFeedback();
+}
---
+++ Assets/DungeonProject/_Scripts/Feedback/FeedbackPlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+class FeedbackPlayer : MonoBehaviour
+{    
+    Feedback[] feedbacks;
+
+    private void Awake()
+    {
+        feedbacks = GetComponents<Feedback>();
+    }
+
+    public void PlayFeedbacks()
+    {
+        CompletePreviousFeedback();
+
+        foreach (Feedback feedback in feedbacks)
+        {
+            feedback.CreateFeedback();
+        }
+    }
+
+    private void CompletePreviousFeedback()
+    {
+        foreach (Feedback feedback in feedbacks)
+        {
+            feedback.ResetFeedback();
+        }
+    }
+}
