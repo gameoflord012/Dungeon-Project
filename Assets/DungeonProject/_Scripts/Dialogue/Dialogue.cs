9989f26877b1a540864b40b5d0f9c92c1e75aa84
Hưng Võ <hungvo@Macbuc.local>
2021-08-16 21:39:43 +0700

    test dialogue and it works

---
+++ Assets/DungeonProject/_Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue : MonoBehaviour
+{
+    [SerializeField] DialogueUI dialogueUI;
+    [SerializeField] string speakerName;
+    [SerializeField] string[] dialogues;
+    [SerializeField] bool startBySpeaker = true;
+
+    private void Start()
+    {
+        dialogueUI.StartDialogue(this);
+    }
+
+    public string GetSpeakerName()
+    {
+        return speakerName;
+    }
+
+    public string[] GetDialogues()
+    {
+        return dialogues;
+    }
+
+    public bool IsStartBySpeaker()
+    {
+        return startBySpeaker;
+    }
+}
--- Assets/DungeonProject/_Scripts/Dialogue/DialogueUI.cs
+++ Assets/DungeonProject/_Scripts/Dialogue/DialogueUI.cs
@@ -10,4 +10,65 @@ public class DialogueUI : MonoBehaviour
     [SerializeField] Button nextButton;
     [SerializeField] Button skipButton;
     [SerializeField] Button quitButton;
+
+    string[] speakerNames = new string[2];
+    string[] dialogues = null;
+    int currentIndex = 0;
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if (dialogue.IsStartBySpeaker())
+        {
+            speakerNames[0] = dialogue.GetSpeakerName();
+            speakerNames[1] = "player";
+        }
+        else
+        {
+            speakerNames[0] = "player";
+            speakerNames[1] = dialogue.GetSpeakerName();
+        }
+
+        dialogues = dialogue.GetDialogues();
+        currentIndex = 0;
+
+        nextButton.gameObject.SetActive(true);
+        skipButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(false);
+        RefreshUI();
+    }
+
+    public void Next()
+    {
+        ++currentIndex;
+        RefreshUI();
+    }
+
+    public void Skip()
+    {
+        currentIndex = dialogues.Length - 1;
+        RefreshUI();
+    }
+
+    public void Quit()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void RefreshUI()
+    {
+        speaker.text = speakerNames[currentIndex % 2];
+        dialogue.text = dialogues[currentIndex];
+
+        if (!HasNext())
+        {
+            nextButton.gameObject.SetActive(false);
+            skipButton.gameObject.SetActive(false);
+            quitButton.gameObject.SetActive(true);
+        }
+    }
+
+    private bool HasNext()
+    {
+        return currentIndex < dialogues.Length - 1;
+    }
 }
