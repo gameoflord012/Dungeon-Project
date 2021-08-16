126f0534c59db93eae06be1601f749c8b7c71049
Hưng Võ <hungvo@Macbuc.local>
2021-08-16 20:11:09 +0700

    create dialogueUI.cs

---
+++ Assets/DungeonProject/_Scripts/Dialogue/DialogueUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUI : MonoBehaviour
+{
+    [SerializeField] Text speaker;
+    [SerializeField] Text dialogue;
+    [SerializeField] Button nextButton;
+    [SerializeField] Button skipButton;
+    [SerializeField] Button quitButton;
+}
