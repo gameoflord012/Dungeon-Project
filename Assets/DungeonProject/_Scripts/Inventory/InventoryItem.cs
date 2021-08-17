20e26535d2d28c0885d8f62dc3922fdc7cca8a57
Hưng Võ <hungvo@Macbuc.local>
2021-08-17 21:24:45 +0700

    create some .cs file about inventory

---
+++ Assets/DungeonProject/_Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory Item", menuName = "Inventory Item", order = 0)]
+public class InventoryItem : ScriptableObject
+{
+    [SerializeField] Sprite icon = null;
+    [SerializeField] bool stackable = false;
+    [SerializeField] float basePrice;
+
+    public Sprite GetIcon()
+    {
+        return icon;
+    }
+
+    public bool IsStackable()
+    {
+        return stackable;
+    }
+
+    public float GetPrice()
+    {
+        return basePrice;
+    }
+}
---
+++ Assets/DungeonProject/_Scripts/Inventory/UI/ItemSlotUI.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlotUI : MonoBehaviour
+{
+    [SerializeField] Image itemIcon;
+    [SerializeField] Text number;
+}
