36565a413f8c4e29a3dcdc484895c7ee20c8ff36
gameoflord012 <manhhieu012@gmail.com>
2021-08-18 15:36:02 +0700

    fix fov rotation

--- Assets/DungeonProject/_Scripts/AI/RotateToPointer.cs
+++ Assets/DungeonProject/_Scripts/AI/RotateToPointer.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class RotateToPointer : MonoBehaviour
 {
-    [SerializeField] float rotateSpeed = 1f;
+    [SerializeField] float rotateSpeed = 1;
+    [SerializeField] float angleOffset = 10;
 
     public void RotateToPointerPosition(Vector2 pointerPosition)
     {
+        StopAllCoroutines();
+        StartCoroutine(RotateRoutine(pointerPosition));
+    }
+
+    public IEnumerator RotateRoutine(Vector2 pointerPosition)
+    {
         Vector2 pointerDirection = pointerPosition - (Vector2)transform.position;
-        transform.right = Vector2.Lerp(transform.right, pointerDirection, rotateSpeed * Time.deltaTime);
+        while(Vector2.Angle(transform.right, pointerDirection) > angleOffset)
+        {
+            transform.right = Vector2.Lerp(transform.right, pointerDirection, rotateSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
--- Assets/DungeonProject/_Scripts/AI/Tasks/LookAroundTask.cs
+++ Assets/DungeonProject/_Scripts/AI/Tasks/LookAroundTask.cs
@@ -3,11 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAroundTask : MonoBehaviour
+public class LookAroundTask : EnemyTaskBase
 {
     [Task]
     public void LookAround(int angle)
     {
+        Vector2 pointerDirection = fov.transform.rotation * Quaternion.Euler(0, 0, angle) * Vector2.right;
+        inputEvents.OnPointerPositionChangedCallback((Vector2)transform.position + pointerDirection);
+        Task.current.Succeed();
 
+        if(Task.isInspected)
+        {
+            Task.current.debugInfo = "Look direction: " + pointerDirection;
+        }
     }
 }
