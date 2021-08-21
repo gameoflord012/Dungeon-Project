c9391d30781b3f5ab93ecd1fdbf4d7e1848038e2
gameoflord012 <manhhieu012@gmail.com>
2021-08-21 17:53:24 +0700

    add some sound

---
+++ Assets/_DungeonProject/_Scripts/Feedback/PlaySoundFeedback.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlaySoundFeedback : Feedback
+{
+    [SerializeField] AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    public override void CreateFeedback()
+    {
+        audioSource.Play();
+    }
+
+    public override void ResetFeedback()
+    {
+        audioSource.Stop();
+    }
+}
